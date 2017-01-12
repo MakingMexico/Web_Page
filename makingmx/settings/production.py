c67a574f2667d85fe71d09eee603c93ad148519f
maumg1196 <maumg1196@gmail.com>
2017-01-11 22:42:11 -0600

    Prove production settings 1

--- makingmx/settings/production.py
+++ makingmx/settings/production.py
@@ -12,3 +12,12 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 DEBUG = False
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = (
+    os.path.join(BASE_DIR, "media")
+)
